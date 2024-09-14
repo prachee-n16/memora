5da43bb2136b443853727b049fe78e200b69b32d
PRACHEE NANDA <p4nanda@uwaterloo.ca>
2024-09-14 12:31:02 -0400

    stub

---
+++ client/src/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
